338219625e9a16fca15e6058efb0cc13a8236464
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2022-04-01 11:49:43 +0100

    Initial commit

---
+++ R/cc.R
@@ -0,0 +1,28 @@
+#' Easy quote characters
+#'
+#' @param ... Unquoted variables (separated by commas) that you wish to quote;
+#'   empty arguments (e.g. third item in `one,two,,four`) will be returned as
+#'   blanks.
+#' @param .clip Should the code to generate the constructed character vector be
+#'   copied to your system clipboard; defaults to TRUE.
+#'
+#' @note The system clipboard will only be used in interactive sessions.
+#'   Outer whitespace is automatically trimmed.
+#'
+#' @return a character vector
+#'
+#' @examples
+#' cc(dale, audrey, laura, hawk)
+#'
+#' @author Tim Taylor
+#'
+#' @importFrom utils capture.output
+#' @export
+cc <- function(..., .clip = TRUE) {
+    res <- vapply(substitute(list(...)), deparse, character(1))
+    res <- res[-1]
+    if (interactive() && .clip && clipr::clipr_available())
+        clipr::write_clip(capture.output(dput(res, control = "all")))
+    res
+}
+
---
+++ inst/tinytest/test_coffee.R
@@ -0,0 +1,5 @@
+# cc
+expect_identical(
+    cc(  dale, audrey, laura  , hawk),
+    c("dale", "audrey", "laura", "hawk")
+)
